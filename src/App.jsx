ea16bb1b899adaf4b7d07946e5f865cd11081908
ArnavU <arnavumarkar.intern@peopleprudent.co.in>
2026-01-21 22:07:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Footer } from "@/layout/Footer"
+import Navbar from "./layout/Navbar"
+import About from "./Sections/About"
+import Contact from "./Sections/Contact"
+import Experience from "./Sections/Experience"
+import Hero from "./Sections/Hero"
+import Projects from "./Sections/Projects"
+import Testimonials from "./Sections/Testimonials"
+
+const App = () => {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        {/* 
+        <Contact /> */}
+      </main>
+      {/* <Footer />  */}
+    </div>
+  )
+}
+export default App
---
+++ src/Sections/Testimonials.jsx
@@ -0,0 +1,9 @@
+import { ChevronLeft, ChevronRight, Quote } from "lucide-react";
+import { useState } from "react";
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+export default Testimonials
